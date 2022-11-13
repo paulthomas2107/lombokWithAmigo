605ba0de5804d40ebaf966896258e19cd5dee3e7
Paul Thomas <paulthomas2107@gmail.com>
2022-11-13 14:26:31 +0000

    More

--- src/main/java/com/pault/lombok/Movie.java
+++ src/main/java/com/pault/lombok/Movie.java
@@ -1,71 +1,32 @@
 package com.pault.lombok;
 
+import lombok.*;
+import lombok.experimental.Accessors;
+
 import java.net.URL;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Movie {
 
+    @NonNull
     private Long id;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public int getRating() {
-        return rating;
-    }
-
-    public void setRating(int rating) {
-        this.rating = rating;
-    }
-
-    public URL getOfficialSite() {
-        return officialSite;
-    }
-
-    public void setOfficialSite(URL officialSite) {
-        this.officialSite = officialSite;
-    }
-
-    public String getLanguage() {
-        return language;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
-    }
-
+    @NonNull
     private String title;
     private String description;
     private String country;
+    @NonNull
     private int rating;
+    @Getter(AccessLevel.PROTECTED)
     private URL officialSite;
     private String language;
+    @Setter(AccessLevel.PRIVATE)
+    @Getter(AccessLevel.PRIVATE)
+    private final int minutes = 120;
+
+    public int getMinutesPlusX(@NonNull int x) {
+        return getMinutes() + x;
+    }
 }
--- src/test/java/com/pault/lombok/MovieTest.java
+++ src/test/java/com/pault/lombok/MovieTest.java
@@ -9,7 +9,7 @@ import java.net.URL;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MovieTest extends Movie {
-
+  
     private Movie movie;
 
     @BeforeEach
@@ -27,58 +27,97 @@ class MovieTest extends Movie {
     @Test
     void testGetId() {
         assertNotNull(movie);
-        assertEquals(1L, getId());
+        assertEquals(1L, movie.getId());
     }
 
     @Test
     void testSetId() {
+        assertNotNull(movie);
+        movie.setId(2L);
+        assertEquals(2, movie.getId());
     }
 
     @Test
     void testGetTitle() {
+        assertNotNull(movie);
+        assertEquals("The Lord of The Rings: The Fellowship of The Ring", movie.getTitle());
     }
 
     @Test
     void testSetTitle() {
+        assertNotNull(movie);
+        movie.setTitle("The House at Downing ~Street");
+        assertEquals("The House at Downing ~Street", movie.getTitle());
     }
 
     @Test
     void testGetDescription() {
+        assertNotNull(movie);
+        assertEquals("Set in Middle Earth with Hobbits and Trolls etc.", movie.getDescription());
     }
 
     @Test
     void testSetDescription() {
+        assertNotNull(movie);
+        movie.setDescription("The usual amount of gore !!!");
+        assertEquals("The usual amount of gore !!!", movie.getDescription());
     }
 
     @Test
     void testGetCountry() {
+        assertNotNull(movie);
+        assertEquals("New Zealand / United States", movie.getCountry());
     }
 
     @Test
     void testSetCountry() {
+        assertNotNull(movie);
+        movie.setCountry("Germany");
+        assertEquals("Germany", movie.getCountry());
     }
 
     @Test
     void testGetRating() {
+        assertNotNull(movie);
+        assertEquals(8, movie.getRating());
     }
 
     @Test
     void testSetRating() {
+        assertNotNull(movie);
+        movie.setRating(129);
+        assertEquals(129, movie.getRating());
     }
 
     @Test
-    void testGetOfficialSite() {
+    void testGetOfficialSite() throws MalformedURLException {
+        assertNotNull(movie);
+        assertEquals(new URL("https://www.lordoftherings.com"), movie.getOfficialSite());
     }
 
     @Test
-    void testSetOfficialSite() {
+    void testSetOfficialSite() throws MalformedURLException {
+        assertNotNull(movie);
+        movie.setOfficialSite(new URL("http://www.gore.net"));
+        assertEquals(new URL("http://www.gore.net"), movie.getOfficialSite());
     }
 
     @Test
     void testGetLanguage() {
+        assertNotNull(movie);
+        assertEquals("English", movie.getLanguage());
     }
 
     @Test
     void testSetLanguage() {
+        assertNotNull(movie);
+        movie.setLanguage("Chinese");
+        assertEquals("Chinese", movie.getLanguage() );
+    }
+
+    @Test
+    void testGetMinutesPlusX() {
+        assertNotNull(movie);
+        assertEquals(121, movie.getMinutesPlusX(1));
     }
 }
